cc5f058cdda0433dfed0b79df35a8c7ecb9f1195
Ethan Pletcher <epletcher@EPI-4ZGKRD2-DT.campus.net.ucsf.edu>
2017-09-03 12:15:24 -0700

    I really have no idea.

--- public/js/home.js
+++ public/js/home.js
@@ -3,3 +3,18 @@ function googleTranslateElementInit() {
         pageLanguage: 'en'
     }, 'google_translate_element');
 }
+
+var quail = {
+    open: "6-8",
+    dogs: 1,
+    fishing: false,
+    hiking: true,
+    Horseback_Riding: true,
+    Mountain_Biking: false,
+    kayaking: false
+}
+
+$.get('/park', quail, function success(data, textStatus) {
+    console.log(data)
+    console.log(textStatus)
+});
